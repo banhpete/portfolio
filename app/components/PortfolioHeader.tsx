20e0d0532628337b0206a3503aed528bf187d1d5
Peter Banh <peter.banh95@gmail.com>
2024-10-16 00:19:18 -0400

    Link header to index page

--- app/components/PortfolioHeader.tsx
+++ app/components/PortfolioHeader.tsx
@@ -1,16 +1,19 @@
 import { Koulen } from "next/font/google";
 import Menu from "./Menu";
+import Link from "next/link";
 
 const koulen = Koulen({ weight: ["400"], subsets: ["latin"] });
 
 export default function PortfolioHeader({}) {
   return (
     <div>
-      <header
-        className={`flex items-center justify-center text-7xl ${koulen.className}`}
-      >
-        PBANH.DEV
-      </header>
+      <Link href="/">
+        <header
+          className={`flex items-center justify-center text-7xl ${koulen.className}`}
+        >
+          PBANH.DEV
+        </header>
+      </Link>
       <Menu />
     </div>
   );
