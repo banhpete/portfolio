59287fd851d99b33ce1d67bde1e93cf2540f258d
Peter Banh <peter.banh95@gmail.com>
2024-10-19 19:15:37 -0400

    Update work experience

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -14,16 +14,14 @@ export default function Button({
   disabled?: boolean | undefined;
 }>) {
   return (
-    <div className={`${className}`}>
-      <button
-        className="disabled:bg-gray-300 disabled:cursor-not-allowed disabled:text-white hover:bg-blue-600 hover:text-white focus:bg-blue-600 focus:text-white w-full p-2 border-black border-2"
-        onClick={onClick}
-        onSubmit={onSubmit}
-        type={type ? type : "button"}
-        disabled={disabled}
-      >
-        {children}
-      </button>
-    </div>
+    <button
+      className={`${className} disabled:bg-gray-300 disabled:cursor-not-allowed disabled:text-white hover:bg-blue-600 hover:text-white focus:bg-blue-600 focus:text-white p-2 border-black border-2`}
+      onClick={onClick}
+      onSubmit={onSubmit}
+      type={type ? type : "button"}
+      disabled={disabled}
+    >
+      {children}
+    </button>
   );
 }
--- app/work/page.tsx
+++ app/work/page.tsx
@@ -12,96 +12,136 @@ import XeroIcon from "./components/icons/XeroIcon";
 
 export default function Work() {
   return (
-    <div className="border-4 flex flex-grow flex-col items-center justify-center bg-gray-400">
-      <Button className="w-5/12 my-4 bg-white">Get my CV</Button>
-      <VerticalTimeline>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          date="Apr 2023 - Present"
-          dateClassName="px-2"
-          iconClassName="bg-white"
-          icon={<A2XIcon className="size-9" />}
-        >
-          <h1 className="text-lg font-semibold">A2X</h1>
-          <h2 className="text-sm font-semibold">Software Developer</h2>
-          <h3 className="text-sm font-light">
-            Skills: Java, Spring Boot, Vue, GCP, SQL, Github Actions
-          </h3>
-          <p className="mt-2 text-sm">
-            Software Development, System Architecture, Customer Success
-          </p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          date="Dec 2020 - Apr 2023"
-          dateClassName="px-4"
-          icon={<XeroIcon className="size-9" />}
-        >
-          <h1 className="text-lg font-semibold">Xero</h1>
-          <h2 className="text-sm font-semibold">
-            Junior/Intermediate Software Developer
-          </h2>
-          <h3 className="text-sm font-light">
-            Utilize: C#, Dotnet, React, Redux, Jenkins, TeamCity, AWS
-          </h3>
-          <p className="mt-2 text-sm">
-            Software Development, System Architecture, Customer Success
-          </p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          date="Oct 2020 - Nov 2020"
-          iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-          icon={<AgsightIcon />}
-        >
-          <h1 className="text-lg font-semibold">AgSights</h1>
-          <h2 className="text-sm font-semibold">Software Developer</h2>
-          <h3 className="text-sm font-light">
-            Utilize: Vanilla Javascript, CSS, HTML, React, Nodejs
-          </h3>
-          <p className="mt-2 text-sm">
-            Software Development, System Architecture, Customer Success
-          </p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--education"
-          date="May 2020 - July 2020"
-          icon={<GeneralAssemblyIcon />}
-        >
-          <h1 className="text-lg font-semibold">General Assembly Bootcamp</h1>
-          <h2 className="text-sm font-semibold">
-            Software Engineering Immersive Student
-          </h2>
-          <h3 className="text-sm font-light">
-            Utilize: Javascript, CSS, HTML, React, Nodejs, Python
-          </h3>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          date="May 2018 - May 2020"
-          icon={<CelesticaIcon />}
-        >
-          <h1 className="text-lg font-semibold">Celestica</h1>
-          <h2 className="text-sm font-semibold">
-            Operations Engineering Associate
-          </h2>
-          <h3 className="text-sm font-light">
-            Utilize: Javascript, CSS, HTML, Google App Scripts
-          </h3>
-          <p>User Experience, Visual Design</p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
-          className="vertical-timeline-element--education"
-          date="2014 - 2018"
-          iconClassName="bg-white"
-          icon={<UofTIcon />}
-        >
-          <h1 className="text-lg font-semibold">University of Toronto</h1>
-          <h2 className="text-sm font-semibold">
-            Materials Science and Engineering Student
-          </h2>
-        </VerticalTimelineElement>
-      </VerticalTimeline>
+    <div className="h-[700px] flex flex-grow flex-col items-center">
+      <h1 className="text-3xl mt-4 text-center">
+        Take a glimpse of what I've done so far
+      </h1>
+      <div className="flex items-center mt-2 mb-8">
+        <h2 className="text-xl">Or take a look at my CVS:&nbsp;&nbsp;</h2>
+        <Button className="p-1">Download My CVS</Button>
+      </div>
+      <div className="overflow-y-scroll flex flex-col w-full scroll">
+        <VerticalTimeline>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date="Apr 2023 - Present"
+            dateClassName="px-2"
+            iconClassName="bg-white"
+            icon={<A2XIcon className="size-9" />}
+          >
+            <h1 className="text-lg font-semibold">A2X</h1>
+            <h2 className="text-sm font-semibold">Software Developer</h2>
+            <ul className="mt-2 marker:text-blue-700 list-disc list-inside text-sm">
+              <li>
+                <span className="font-semibold">Technologies Used:&nbsp;</span>
+                Java, Spring Boot, Vue, GCP, SQL, Github Actions
+              </li>
+              <li>
+                Scope, develop and release new features for the Amazon
+                integration and NetSuite integration at A2X
+              </li>
+              <li>Maintain and optimize the Amazon integration.</li>
+              <li>
+                Improve developer experience by introducing new tools and
+                documentation
+              </li>
+            </ul>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date="Dec 2020 - Apr 2023"
+            dateClassName="px-4"
+            icon={<XeroIcon className="size-9" />}
+          >
+            <h1 className="text-lg font-semibold">Xero</h1>
+            <h2 className="text-sm font-semibold">
+              Junior/Intermediate Software Developer
+            </h2>
+            <ul className="mt-2 marker:text-blue-700 list-disc list-inside text-sm">
+              <li>
+                <span className="font-semibold">Technologies Used:&nbsp;</span>
+                C#, Dotnet, React, Redux, Jenkins, TeamCity, AWS
+              </li>
+              <li>
+                Helped with the development and release of eInvoicing at Xero
+              </li>
+              <li>Led the scaling project for eInvoicing receiving</li>
+              <li>
+                Created the BFF for Xero's new Bill's page ensuring the project
+                is ready for development
+              </li>
+            </ul>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date="Oct 2020 - Nov 2020"
+            iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+            icon={<AgsightIcon />}
+          >
+            <h1 className="text-lg font-semibold">AgSights</h1>
+            <h2 className="text-sm font-semibold">Software Developer</h2>
+            <ul className="mt-2 marker:text-blue-700 list-disc list-inside text-sm">
+              <li>
+                <span className="font-semibold">Technologies Used:&nbsp;</span>
+                Javascript, CSS, HTML, React, Nodejs
+              </li>
+              <li>
+                Developed and improved AgSights bioLinks by integrating APIs and
+                improving the UX
+              </li>
+            </ul>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--education"
+            date="May 2020 - July 2020"
+            icon={<GeneralAssemblyIcon />}
+          >
+            <h1 className="text-lg font-semibold">General Assembly Bootcamp</h1>
+            <h2 className="text-sm font-semibold">
+              Software Engineering Immersive Student
+            </h2>
+            <ul className="mt-2 marker:text-blue-700 list-disc list-inside text-sm">
+              <li>
+                <span className="font-semibold">
+                  Technologies Learned:&nbsp;
+                </span>
+                Javascript, CSS, HTML, React, Nodejs, Python
+              </li>
+            </ul>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date="May 2018 - May 2020"
+            icon={<CelesticaIcon />}
+          >
+            <h1 className="text-lg font-semibold">Celestica</h1>
+            <h2 className="text-sm font-semibold">
+              Operations Engineering Associate
+            </h2>
+            <ul className="mt-2 marker:text-blue-700 list-disc list-inside text-sm">
+              <li>
+                <span className="font-semibold">Technologies Used:&nbsp;</span>
+                Javascript, CSS, HTML
+              </li>
+              <li>
+                Took initiative to learn Javascript, CSS, HTML to create an
+                online statistical process control chart
+              </li>
+            </ul>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--education"
+            date="2014 - 2018"
+            iconClassName="bg-white"
+            icon={<UofTIcon />}
+          >
+            <h1 className="text-lg font-semibold">University of Toronto</h1>
+            <h2 className="text-sm font-semibold">
+              Materials Science and Engineering Student
+            </h2>
+          </VerticalTimelineElement>
+        </VerticalTimeline>
+      </div>
     </div>
   );
 }
