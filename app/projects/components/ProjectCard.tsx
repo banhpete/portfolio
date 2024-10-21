d4a6e7d63ac0a69609fb96f3598e501cbc03cae5
Peter Banh <peter.banh95@gmail.com>
2024-10-21 00:43:24 -0400

    Revamp project page

--- app/projects/components/ProjectCard.tsx
+++ app/projects/components/ProjectCard.tsx
@@ -5,6 +5,7 @@ export default function ProjectCard({
   technologiesUsed,
   status,
   src,
+  projectLink,
   altForImage,
   children,
   className,
@@ -13,25 +14,56 @@ export default function ProjectCard({
   technologiesUsed: string;
   status: "WIP" | "Completed" | "Stable Release";
   src: string;
+  projectLink: string;
   altForImage: string;
-  children: string;
+  children: string | React.ReactNode;
   className?: string;
 }>) {
   return (
-    <div
-      className={`${className} h-[200px] min-h-[200px] border-4 rounded-md flex p-2`}
-    >
-      <div className={"min-w-[250px] w-[250px] content-center border-2"}>
-        <Image src={src} alt={altForImage} width={250} height={200}></Image>
-      </div>
-      <div className={"ml-4 flex-col"}>
-        <div className="flex text-center">
-          <h1 className="text-lg font-medium">{title}</h1>
-          <h2 className="ml-2 text-base leading-7">({status})</h2>
+    <a href={projectLink} className="group" rel="noopener noreferrer">
+      <div
+        className={`${className} min-h-[200px] text-white bg-slate-700 rounded-md flex justify-between p-4 group-hover:scale-105`}
+      >
+        <div className={"min-w-[250px] w-[250px] content-center"}>
+          <Image
+            className="rounded-lg"
+            src={src}
+            alt={altForImage}
+            width={250}
+            height={200}
+          ></Image>
         </div>
-        <h3>Technologies Used: {technologiesUsed}</h3>
-        <p>{children}</p>
+        <div className={"ml-6 flex py-1 flex-col flex-1"}>
+          <div className="flex mt-2 mb-2 text-center">
+            <h1 className="text-lg font-medium">{title}</h1>
+            <h2
+              className={`rounded-lg ml-2 font-semibold text-[0.65rem] px-2 leading-7 ${
+                status === "WIP"
+                  ? "bg-red-500"
+                  : status === "Completed"
+                  ? "bg-green-500"
+                  : "bg-blue-600"
+              }`}
+            >
+              {status}
+            </h2>
+          </div>
+          <p className="text-sm flex-1">{children}</p>
+          <p className="mt-2 text-sm">
+            {technologiesUsed.split(",").map((tech) => (
+              <>
+                <code className="px-1 text-black">{tech.replace(" ", "")}</code>{" "}
+              </>
+            ))}
+          </p>
+        </div>
+        <div className="ml-4 w-3 text-5xl flex justify-end items-center">
+          <div className="relative">
+            <span className="">&gt;</span>
+            <div className="group-hover:translate-y-10 group-focus:translate-y-10 transition ease-linear duration-400 bg-slate-700 absolute top-0 left-0 h-full w-full"></div>
+          </div>
+        </div>
       </div>
-    </div>
+    </a>
   );
 }
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,47 +2,70 @@ import ProjectCard from "./components/ProjectCard";
 
 export default function Projects() {
   return (
-    <div className="border-4 h-[675px] flex flex-col flex-grow p-4 overflow-y-scroll">
-      <ProjectCard
-        title="Portfolio Page"
-        technologiesUsed="Next.js"
-        status="Stable Release"
-        src="/images/portfolio.png"
-        altForImage="Screenshot of portfolio page"
-        className="my-2"
-      >
-        Project test
-      </ProjectCard>
-      <ProjectCard
-        className="my-2"
-        title="Minesweeper Vs."
-        status="WIP"
-        src="/images/mario-minesweeper.jpeg"
-        altForImage="Image of mario minesweeper"
-        technologiesUsed="React, NodeJS, Java and Springboot, C# and Dotnet"
-      >
-        Project test
-      </ProjectCard>
-      <ProjectCard
-        className="my-2"
-        title="Space Chicken"
-        status="Completed"
-        src="/images/space-chicken.jpg"
-        altForImage="Image of Space Chicken"
-        technologiesUsed="Phaser framework, Javascript, CSS, HTML"
-      >
-        Project test
-      </ProjectCard>
-      <ProjectCard
-        className="my-2"
-        title="Mario Inspired Minesweeper"
-        status="Completed"
-        src="/images/mario-minesweeper.jpeg"
-        altForImage="Image of mario minesweeper"
-        technologiesUsed="Javascript, CSS, HTML"
-      >
-        Project test
-      </ProjectCard>
+    <div className="flex flex-col flex-grow p-4">
+      <h1 className="text-3xl mt-4 mb-8 text-center">
+        <span className="text-blue-800">Random</span> projects I&apos;ve worked
+        on
+      </h1>
+      <div className="flex flex-col">
+        <ProjectCard
+          title="Portfolio Page"
+          status="Stable Release"
+          src="/images/portfolio.png"
+          projectLink={"https://pbanh.dev/"}
+          altForImage="Screenshot of portfolio page"
+          className="my-2"
+          technologiesUsed="Next.js, Three.js"
+        >
+          My portfolio page which you are visiting right now! I designed and
+          created it myself using Figma and Next.js. A fun project as I got to
+          learn more about how Next.js works and I got to experiment with
+          Three.js. Please enjoy!
+        </ProjectCard>
+        <ProjectCard
+          className="my-2"
+          title="Minesweeper Vs."
+          status="WIP"
+          src="/images/msv.png"
+          projectLink="https://minesweeperversus.pbanh.dev"
+          altForImage="Image of minesweeper versus"
+          technologiesUsed="React, NodeJS, Java, Springboot, C#, Dotnet, Docker, Circle CI, GCP, AWS"
+        >
+          An on-going project to create an online Minesweeper platform where
+          players can play each other or play by themselves. The purpose of this
+          project was to experiment with different technologies that I wanted to
+          learn more about OR I wanted more experience with such as Websockets,
+          and GCP.
+        </ProjectCard>
+        <ProjectCard
+          className="my-2"
+          title="Space Chicken"
+          status="Completed"
+          src="/images/space-chicken.jpg"
+          projectLink="https://banhpete.github.io/Mintbean-Gamehack-SciFi-OuterSpace/"
+          altForImage="Image of Space Chicken"
+          technologiesUsed="Phaser.js, Javascript, CSS, HTML"
+        >
+          A fun project created during a hackathon hosted by Mintbean (An online
+          community that used to host one day hackathons) where we had to use
+          Phaser JS to make a game. Space Chicken placed 1st in the short
+          hackathon.
+        </ProjectCard>
+        <ProjectCard
+          className="my-2"
+          title="Mario Inspired Minesweeper"
+          status="Completed"
+          src="/images/mario-minesweeper.jpeg"
+          projectLink="https://banhpete.github.io/Minesweeper-SEI-Project/"
+          altForImage="Image of mario minesweeper"
+          technologiesUsed="Javascript, CSS, HTML"
+        >
+          A version of minesweeper made during my time at the General Assembly
+          SEI Bootcamp. One of my first and funniest projects at General
+          Assembly as it challenged me to create and design a version of
+          Minesweeper with a twist and a fun user experience.
+        </ProjectCard>
+      </div>
     </div>
   );
 }
